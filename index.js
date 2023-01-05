831209554b3d0e808aaaec2212d3f980dc5bc197
Raj <singhraj1553@outlook.com>
2023-01-05 22:38:15 +0530

    added backlit

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ grid.addEventListener('mousemove', function(e){
     light.style.left=`${e.clientX}px`;
     light.style.top=`${e.clientY}px`;
 });
+
+// disabling inspect element
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault(); //this prevents right click
+});
+document.onkeydown=function(e){
+    if(event.keycode==123){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="I".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="C".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="J".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode=="U".charCodeAt(0)){
+        return false;
+    }
+};
